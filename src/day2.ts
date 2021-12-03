dae0ad19f44d3592e8fa861af6d1ed8e7330331d
Chris Smith <cs24@sanger.ac.uk>
2021-12-03 08:37:31 +0000

    day 2 part 2

--- src/day2.ts
+++ src/day2.ts
@@ -2,22 +2,28 @@ import { readFileSync } from "fs";
 
 type HorizontalPosition = number;
 type Depth = number;
-type Position = [HorizontalPosition, Depth];
+type Aim = number;
+type Position = [HorizontalPosition, Depth, Aim];
 
 function move(commands: Array<string>, position: Position): Position {
   if (commands.length === 0) {
     return position;
   } else {
+    const [hzp, depth, aim] = position;
     const command = commands[0];
     const [direction, value] = command.split(" ");
     const valueInt = parseInt(value);
 
     if (direction === "forward") {
-      return move(commands.slice(1), [position[0] + valueInt, position[1]]);
+      return move(commands.slice(1), [
+        hzp + valueInt,
+        depth + aim * valueInt,
+        aim,
+      ]);
     } else if (direction === "down") {
-      return move(commands.slice(1), [position[0], position[1] + valueInt]);
+      return move(commands.slice(1), [hzp, depth, aim + valueInt]);
     } else if (direction === "up") {
-      return move(commands.slice(1), [position[0], position[1] - valueInt]);
+      return move(commands.slice(1), [hzp, depth, aim - valueInt]);
     } else {
       throw new Error(`We don't know how to move ${direction}!`);
     }
@@ -33,11 +39,11 @@ const test = [
   "forward 2",
 ];
 
-const testMove = move(test, [0, 0]);
-console.log(testMove[0] * testMove[1], "should equal 150");
+const testMove = move(test, [0, 0, 0]);
+console.log(testMove[0] * testMove[1], "should equal 900");
 
 const input = readFileSync("./src/day2.input.txt", "utf8").split("\n");
 
-const position = move(input, [0, 0]);
+const position = move(input, [0, 0, 0]);
 
 console.log("Answer: ", position[0] * position[1]);
