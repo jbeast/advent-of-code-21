56f2aa56d8cc075ca8cb806fc13469d33ef954ec
Chris Smith <cs24@sanger.ac.uk>
2021-12-09 12:16:56 +0000

    Day 7 part 2

--- src/day7/day7.ts
+++ src/day7/day7.ts
@@ -1,13 +1,31 @@
-import { curry, map, max, min, range, sum, unary } from "ramda";
+import { curry, map, max, min, pipe, range, sum, unary } from "ramda";
 import { readFileSync } from "fs";
 
-const difference = curry(function (a: number, b: number): number {
-  if (a >= b) {
-    return a - b;
-  } else {
-    return b - a;
-  }
-});
+/**
+ * Find the difference between 2 numbers
+ * @returns the difference between 2 numbers
+ */
+function _difference(): (a: number, b: number) => number {
+  return function (a: number, b: number): number {
+    return max(a, b) - min(a, b);
+  };
+}
+
+const difference = curry(_difference());
+
+/**
+ * Find the sum of the integers between two integers
+ * @param a an integer
+ * @param b an integer
+ * @returns the sum of the integers between a and b
+ */
+function _sum_between(a: number, b: number): number {
+  const minimum = min(a, b);
+  const maximum = max(a, b);
+  return ((maximum - minimum + 1) * (minimum + maximum)) / 2;
+}
+
+const sumBetween = curry(_sum_between);
 
 /**
  * Finds the cheapest horizontal position for crabs to align on
@@ -19,9 +37,14 @@ function findCheapestOutcome(list: Array<number>): number {
 
   return range(minHorizontalPosition, maxHorizontalPosition).reduce(
     (memo, value) => {
-      return min(sum(map<number, number>(difference(value))(list)), memo);
+      return pipe(
+        map<number, number>(difference(value)),
+        map<number, number>(sumBetween(1)),
+        sum,
+        min(memo)
+      )(list);
     },
-    0
+    Infinity
   );
 }
 
